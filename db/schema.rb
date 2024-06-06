db95c10e41e77681c0fd9bbfd29dd4bceed1b5fc
Alexis <alexis@mailpro3.ventadis.fr>
2024-06-06 11:23:30 +0200

    init rails + scaffold
    Signed-off-by: Alexis <alexis@mailpro3.ventadis.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_06_091911) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_06_091955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(name: "Light & Spacious Garden Flat London", address: "10 Clifton Gardens London W9 1DT", price: 1000)
+Flat.create!(name: "Light & Spacious Garden Flat Paris", address: "10 Clifton Gardens Paris W9 1DT", price: 2000)
+Flat.create!(name: "Light & Spacious Garden Flat Berlin", address: "10 Clifton Gardens Berlin W9 1DT", price: 3000)
+Flat.create!(name: "Light & Spacious Garden Flat Madrid", address: "10 Clifton Gardens Madrid W9 1DT", price: 4000)
+Flat.create!(name: "Light & Spacious Garden Flat Rome", address: "10 Clifton Gardens Rome W9 1DT", price: 5000)
+Flat.create!(name: "Light & Spacious Garden Flat Amsterdam", address: "10 Clifton Gardens Amsterdam W9 1DT", price: 6000)
